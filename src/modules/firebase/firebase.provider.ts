3f8f8a631f7e4f22e5642df87ef46df7bec1487c
stafno tapia <psrichard30@gmail.com>
2026-02-10 21:19:44 -0600

    Update Firebase config to support env variables

--- src/modules/firebase/firebase.provider.ts
+++ src/modules/firebase/firebase.provider.ts
@@ -1,18 +1,32 @@
-
-import * as admin from 'firebase-admin'
-import * as path from 'path'
+import * as admin from 'firebase-admin';
+import * as path from 'path';
 
 export const firebaseAdminProvider = {
   provide: 'FIREBASE_ADMIN',
   useFactory: () => {
     if (!admin.apps.length) {
-      const serviceAccountPath = path.join(__dirname, '../../../push-wep-app-firebase-admin.json')
+      
+      if (process.env.FIREBASE_PROJECT_ID) {
+        admin.initializeApp({
+          credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+          }),
+        });
+      }
+     
+      else {
+        const serviceAccountPath = path.join(
+          __dirname,
+          '../../../firebase-admin.json'
+        );
 
-      admin.initializeApp({
-        credential: admin.credential.cert(serviceAccountPath),
-      })
-
+        admin.initializeApp({
+          credential: admin.credential.cert(serviceAccountPath),
+        });
+      }
     }
-    return admin
+    return admin;
   },
-}
+};
