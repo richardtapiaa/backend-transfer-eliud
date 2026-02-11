c7fa3b3252dfc2e908ee94a6609656e8d8af8726
stafno tapia <psrichard30@gmail.com>
2026-02-10 22:51:09 -0600

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     
   // configuracion de CORS
   app.enableCors({
-    origin: ['http://localhost:4321', 'http://localhost:5173'],
+    origin: ['http://localhost:4321', 'https://transfereliud.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
